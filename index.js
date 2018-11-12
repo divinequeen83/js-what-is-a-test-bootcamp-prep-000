572f66395776c769f086201cd01e13953915e2b4
Kimberly James <kimberlync1983@yahoo.com>
2018-11-12 20:54:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 // write your code below!
+console . log
+var name = "Joe"
+
 var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+console . log
+console . log
+var name = "joe"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
